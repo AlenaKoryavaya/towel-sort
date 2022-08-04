30437a65c4b98eebc0ec07a75b2c32f2fe230a38
Alena Koryavaya <elena.koryavaya@gmail.com>
2022-08-04 14:10:41 +0300

    added changes to the index.js file (the task - completed)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+module.exports = function towelSort(matrix) {
+    if (typeof matrix === "undefined") {
+        return [];
+    } else {
+        let sortedMatrix = [];
+        // Сортируем массивы внутри массива
+        matrix.forEach((elem, index) => {
+            (index + 2) % 2 === 0
+                ? sortedMatrix.push(elem)
+                : sortedMatrix.push(elem.sort((a, b) => b - a));
+        });
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+        let oneCommanArray = [];
+        // Превращаем отсортированный саммив массивов в один массив
+        sortedMatrix.forEach((elem) => oneCommanArray.push(...elem));
+        return oneCommanArray;
+    }
+};
